c147b81dcc6fa2f97ad9b5f0ca2315d121dbdb28
SrMoney <changfindaway@gmail.com>
2022-08-26 13:58:13 -0300

    commit inicial

---
+++ formulario/scripts.js
@@ -0,0 +1,93 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordConfirmation = document.getElementById("password-confirmation");
+
+form.addEventListener("submit" , (e) => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs () {
+    const usernameValue = username.value;
+    const emailValue = email.value;
+    const passwordValue = password.value;
+    const passwordConfirmationValue = passwordConfirmation.value;
+
+    if (usernameValue === "") {
+        setErrorFor(username, "O nome de usuario eh obrigatorio.");
+    } else {
+        setSuccessFor(username);
+    }
+
+    if (emailValue === "") {
+        setErrorFor(email, "O email eh obrigatorio");
+    } else if (!checkEmail(emailValue)) {
+        setErrorFor(email, "Por favor, insira um email valido.");
+    } else {
+        setSuccessFor(email);   
+    }
+
+    if (passwordValue === "") {
+        setErrorFor(password, "A senha e obrigatoria.");
+    } else if (passwordValue.length < 7) {
+        setErrorFor(password," A senha precisa ter no minimo 7 caracteres. ");
+    } else {
+        setSuccessFor(password);
+    }
+
+    if (passwordConfirmationValue === "") {
+        setErrorFor(passwordConfirmation, "A confirmacao de senha eh obrigatoria")
+    } else if (passwordConfirmationValue !== passwordValue) {
+        setErrorFor(passwordConfirmation, "As senhas nao conferem");
+    } else {
+        setSuccessFor(passwordConfirmation);
+    }
+
+    const formControls = form.querySelectorAll(".form-control");
+
+    const formIsValid = [...formControls].every((formControl) => {
+        return formControl.className === "form-control success";
+    });
+
+    if (formIsValid) {
+        console.log("O formulario esta 100% valido!");
+    }
+}
+
+
+function setErrorFor(input, message ) { 
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+
+    //add msg de erro
+
+    small.innerText = message;
+
+    // add class de erro
+
+    formControl.className = "form-control error";
+
+}
+
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+
+    // adicionar a classe de sucesso
+
+    formControl.className = "form-control success";
+
+
+
+}
+
+
+
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    );
+}
